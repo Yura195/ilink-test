aee7a889317658391cd82f531231b05cd7ca1f8c
Yura195 <tereshin_95@mail.ru>
2021-09-12 23:18:42 +0700

    feat:create CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() userDto: CreateUserDto) {
+    const user = await this.userService.create(userDto);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'User created successfully',
+      user,
+    };
+  }
+
+  @Get()
+  async getAllUsers() {
+    const users = await this.userService.getAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Users fetched successfully',
+      users,
+    };
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: number, @Body() userDto: CreateUserDto) {
+    await this.userService.update(id, userDto);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'User updated successfully',
+    };
+  }
+
+  @Get('user')
+  async getUsersBy(@Query() query) {
+    const users = await this.userService.getUsersByParams(query);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Users fetched successfully',
+      users,
+    };
+  }
+
+  @Get('restore/:id')
+  async restoreUser(@Param('id') id: number) {
+    return await this.userService.restore(id);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number) {
+    return this.userService.delete(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,40 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService  {
-    
+export class UsersService{
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto) {
+    const user = this.userRepository.create(dto);
+    await this.userRepository.save(dto);
+    return user;
+  }
+
+  async getAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async update(id: number, dto: CreateUserDto) {
+    await this.userRepository.update({ id }, dto);
+    return await this.userRepository.findOne({ id });
+  }
+
+  async getUsersByParams(param:any): Promise<User[]> {
+    return await this.userRepository.find(param);
+  }
+
+  async delete(id:number) {
+    return this.userRepository.softDelete(id);
+  }
+
+  async restore(id:number) {
+    return this.userRepository.restore(id);
+  }
 }
